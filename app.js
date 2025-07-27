3f52254a534ec7b50fef423819e9cfa428402e1f
Amit Saini <amitsaini.ce@gmail.com>
2025-07-27 19:08:17 +0530

    Implemented localStorage to save and retrieve tasks

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 let input = document.querySelector("input");
 let addTaskBtn = document.querySelector("#add-task-btn");
 let list = document.querySelector("ol");
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
+
+for (task of tasks) {
+    let li = document.createElement("li");
+    li.innerText = task;
+    let deleteBtn = addBtn();
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+}
 
 addTaskBtn.addEventListener("click", function () {
 
     if (input.value.trim() === "") return;
 
+
     let li = document.createElement("li");
-    li.innerText = `${input.value}`
+    tasks.push(input.value);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    li.innerText = input.value;
     list.appendChild(li);
 
     let deleteBtn = addBtn();
     li.appendChild(deleteBtn);
 
     input.value = "";
+    console.log(task);
 
 })
 
@@ -25,9 +38,15 @@ function addBtn() {
     return btn;
 }
 
-list.addEventListener("click",function(details){
-    if(details.target.nodeName=="BUTTON"){
-        let listItem=details.target.parentElement;
+list.addEventListener("click", function (details) {
+    if (details.target.nodeName == "BUTTON") {
+        let listItem = details.target.parentElement;
+        let taskTest = listItem.firstChild.textContent.trim();
+        let index = tasks.indexOf(taskTest);
+        if (index > -1) {
+            tasks.splice(index, 1);
+        }
+        localStorage.setItem("tasks", JSON.stringify(tasks));
         listItem.remove();
     }
 })
